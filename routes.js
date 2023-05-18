5a6e18449b5be445bf05c660bd95c74f813dfb03
Renan Caldas <renan@renancaldas.dev.br>
2023-05-18 16:34:46 -0300

    feat: adicionado rota de login

--- routes.js
+++ routes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
 const route = express.Router();
-const homeController = require("./src/controllers/homeControllers");
-const contatoController = require("./src/controllers/contatoController");
+const homeController = require("./src/controllers/homeController");
+const loginController = require("./src/controllers/loginController");
 
 // Rotas da home
-route.get("/", homeController.paginaInicial);
-route.post("/", homeController.trataPost);
+route.get("/", homeController.index);
 
-// Rotas do contato
-route.get("/contato", contatoController.paginaInicial);
+// Rotad de login
+route.get("/login/index", loginController.index);
 
 module.exports = route;
