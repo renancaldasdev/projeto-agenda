b1d5b27550b70c350fd027452a201027d06637fb
Renan Caldas <renan@renancaldas.dev.br>
2023-05-18 16:35:49 -0300

    feat: controllers da home e de login

--- src/controllers/contatoController.js
+++
@@ -1,3 +0,0 @@
-exports.paginaInicial = (req, res) => {
-  res.send("<h1>Obrigado por entrar em contato</h1");
-};
--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -11,6 +11,8 @@ exports.checkCsrfError = (err, req, res, next) => {
   if (err && "EBADCSRFTOKEN" === err.code) {
     return res.render("404");
   }
+
+  next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
