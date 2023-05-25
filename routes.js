509e2e97db439da57e5d53d7695db4844fe37888
Renan Caldas <renan@renancaldas.dev.br>
2023-05-25 19:28:00 -0300

    :sparkles: feat: criado rotas de contato, registrar contato e editar contato

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
+
+const { loginRequired } = require("./src/middlewares/middlewares");
 
 // Rotas da home
 route.get("/", homeController.index);
@@ -15,5 +18,11 @@ route.post("/login/register", loginController.register);
 route.post("/login/login", loginController.login);
 // Criando rota de logout
 route.get("/login/logout", loginController.logout);
+// Criando rota de contato
+route.get("/contato/index", loginRequired, contatoController.index);
+// Criando rota de cadastro de usuários
+route.post("/contato/register", loginRequired, contatoController.register);
+// Criando rota de edição de usuários
+route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
 
 module.exports = route;
